b1cfc1e49b772850db384c0c4421a0bb205d52fc
Juliano Luiz Fernandes <julianolf@MacBook-Pro-de-Juliano.local>
2016-03-26 21:07:33 -0300

    Here it goes my initial dotfiles and setup scripts. For now it just supports Mac OS X system.

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+	# rsync	--exclude ".git/" \
+	# 		--exclude ".DS_Store" \
+	# 		--exclude "bootstrap.sh" \
+	# 		--exclude "README.md" \
+	# 		--exclude "LICENSE" \
+	# 		-avh --no-perms . ~;
+
+	# Choose proper installation script
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+		source ./linux_install.sh;
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		source ./mac_install.sh
+	else
+		echo "Sorry, OS not supported yet";
+	fi;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
