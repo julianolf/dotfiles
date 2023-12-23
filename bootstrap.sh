eeedf1809ca86ca506e22f85e1ba7b6a79102f42
Juliano Fernandes <julianofernandes@gmail.com>
2023-12-23 14:09:30 -0300

    Refactor bootstrap to just execute rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,9 @@
 #!/usr/bin/env zsh
 
-cd "$(dirname "${(%):-%x}")"
-
-git pull origin main;
-
-function doIt() {
-	rsync	--exclude ".git" \
-		--exclude ".gitignore" \
-		--exclude ".gitmodules" \
-		--exclude ".editorconfig" \
-		--exclude ".DS_Store" \
-		--exclude "README.md" \
-		--exclude "LICENSE" \
-		--exclude "bootstrap.sh" \
-		--exclude "brew.sh" \
-		--exclude "mac_install.sh" \
-		-avh --no-perms . ~;
-
-	source ./mac_install.sh
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-
-unset doIt;
+rsync	--exclude ".git" \
+	--exclude ".gitignore" \
+	--exclude ".DS_Store" \
+	--exclude "configure" \
+	--exclude "LICENSE" \
+	--exclude "README.md" \
+	-avh --no-perms . ~
