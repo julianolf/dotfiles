d458ab209eea3d331a9a23e1d919655fe630b3f9
Juliano Fernandes <julianofernandes@gmail.com>
2023-12-23 12:31:34 -0300

    Remove Rust installation and unnecessary message output

--- mac_install.sh
+++ mac_install.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env zsh
 
-# Installation script for Mac OS Systems
-echo "Running Mac OS installation";
+echo "Running installation";
 
 # Install Xcode command line tools
 xcode-select --install
-
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Use Homebrew for your own sanity
 $SHELL -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -16,5 +12,3 @@ if which brew > /dev/null; then
 	# Install command-line tools using Homebrew
 	source ./brew.sh
 fi;
-
-echo "Done. Note that some of these changes requires you to reopen some applications or even logout/restart the system to take effect."
