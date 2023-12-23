195e0f9eda0e60d55eb4c39529b7ff3dd5aae25b
Juliano Fernandes <julianofernandes@gmail.com>
2023-12-23 12:30:16 -0300

    Keep only the basics

--- brew.sh
+++ brew.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env zsh
 
-# Update packages list.
 brew update
-
-# Upgrade any already-installed formulae.
 brew upgrade
-
-# Install some other useful tools.
-brew install htop tmux neovim universal-ctags jq rgbds pyenv qemu awscli aws-sam-cli
-
-# Remove outdated versions from the cellar.
+brew install tmux neovim jq pyenv
 brew cleanup
