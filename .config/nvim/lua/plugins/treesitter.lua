ac3c501a8ca828b5b784178c6783f19c629db5b3
Juliano Fernandes <julianofernandes@gmail.com>
2023-12-24 14:17:01 -0300

    Make sure treesitter installs some extra programming languages grammar

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+   "nvim-treesitter/nvim-treesitter",
+   opts = {
+      ensure_installed = {
+         "c_sharp",
+         "cpp",
+         "csv",
+         "dockerfile",
+         "go",
+         "gomod",
+         "hcl",
+         "make",
+         "rst",
+         "ruby",
+         "rust",
+         "sql",
+         "ssh_config",
+         "terraform",
+      },
+   },
+}
