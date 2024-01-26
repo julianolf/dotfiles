f19af922a722ae7ab181b00f159c482cecd53be7
Juliano Fernandes <julianofernandes@gmail.com>
2024-01-26 17:49:16 -0300

    Remove unused list of args on Telescope configuration function

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
       dependencies = {
          "nvim-lua/plenary.nvim"
       },
-      config = function(_, opts)
+      config = function()
          local builtin = require("telescope.builtin")
 
          vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
