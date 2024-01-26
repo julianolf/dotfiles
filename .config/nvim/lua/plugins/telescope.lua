8a37e38c9340cf5133baae0d7cff0773d0b975eb
Juliano Fernandes <julianofernandes@gmail.com>
2024-01-25 21:52:53 -0300

    Add Telescope to the plugins list

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+   {
+      "nvim-telescope/telescope.nvim",
+      dependencies = {
+         "nvim-lua/plenary.nvim"
+      },
+      config = function(_, opts)
+         local builtin = require("telescope.builtin")
+
+         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+         vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+         vim.keymap.set("n", "<leader>fG", builtin.live_grep, {})
+         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      end,
+   },
+}
