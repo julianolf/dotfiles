f0dfda172e7b47ce44d813355e5018197fb36ce3
Juliano Fernandes <julianofernandes@gmail.com>
2024-02-05 15:36:24 -0300

    Fix treesitter configuration for proper initialization

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -43,6 +43,26 @@ return {
             "vimdoc",
             "yaml",
          }
-      }
+      },
+      init = function (plugin)
+         require("lazy.core.loader").add_to_rtp(plugin)
+         require("nvim-treesitter.query_predicates")
+      end,
+      config = function (_, opts)
+         if type(opts.ensure_installed) == "table" then
+            local added = {}
+            opts.ensure_installed = vim.tbl_filter(
+               function (lang)
+                  if added[lang] then
+                     return false
+                  end
+                  added[lang] = true
+                  return true
+               end,
+               opts.ensure_installed
+            )
+         end
+         require("nvim-treesitter.configs").setup(opts)
+      end,
    }
 }
