dfa72eaac42dde562a0503c6699d071004db77a2
Juliano Fernandes <julianofernandes@gmail.com>
2024-02-19 16:41:45 -0300

    Apply code formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,22 +4,27 @@ return {
       dependencies = {
          "williamboman/mason-lspconfig.nvim",
          "williamboman/mason.nvim",
-         { "hrsh7th/cmp-nvim-lsp", dependencies = { "hrsh7th/nvim-cmp" }},
+         { "hrsh7th/cmp-nvim-lsp", dependencies = { "hrsh7th/nvim-cmp" } },
       },
       config = function()
          local lspconfig = require("lspconfig")
-         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
          local on_attach = function()
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0, desc="Show documentation"})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0, desc="Go-to definition"})
-            vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0, desc="Go-to type definition"})
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0, desc="Go-to implementaion"})
-            vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0, desc="Go-to next issue"})
-            vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0, desc="Go-to previous issue"})
-            vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0, desc="Fuzzy find issues"})
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {buffer=0, desc="LSP Rename"})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0, desc="LSP code actions"})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Show documentation" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0, desc = "Go-to definition" })
+            vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0, desc = "Go-to type definition" })
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0, desc = "Go-to implementaion" })
+            vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0, desc = "Go-to next issue" })
+            vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0, desc = "Go-to previous issue" })
+            vim.keymap.set(
+               "n",
+               "<leader>dl",
+               "<cmd>Telescope diagnostics<cr>",
+               { buffer = 0, desc = "Fuzzy find issues" }
+            )
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0, desc = "LSP Rename" })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0, desc = "LSP code actions" })
          end
 
          lspconfig.lua_ls.setup({
@@ -35,7 +40,7 @@ return {
          })
 
          lspconfig.gopls.setup({ capabilities = capabilities, on_attach = on_attach })
-         lspconfig.pyright.setup({ capabilities = capabilities,  on_attach = on_attach })
+         lspconfig.pyright.setup({ capabilities = capabilities, on_attach = on_attach })
          lspconfig.rust_analyzer.setup({ capabilities = capabilities, on_attach = on_attach })
          lspconfig.terraformls.setup({ capabilities = capabilities, on_attach = on_attach })
       end,
@@ -48,8 +53,8 @@ return {
             "gopls",
             "lua_ls",
             "pyright",
-            "terraformls",
             "rust_analyzer",
+            "terraformls",
          },
       },
    },
@@ -59,8 +64,8 @@ return {
       build = ":MasonUpdate",
       opts = {
          ensure_installed = {
-            "stylua",
             "shfmt",
+            "stylua",
          },
       },
    },
