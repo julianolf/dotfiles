8482050159a0fb2df60ae8cd7d2330b4a6d057e8
Juliano Fernandes <julianofernandes@gmail.com>
2024-03-06 19:45:01 -0300

    Set a bunch of vim options

--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -1,11 +1,20 @@
-vim.opt.backup = false
 vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
 vim.opt.cursorline = true
 vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
+vim.opt.breakindent = true
 vim.opt.smartindent = true
 vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.undofile = true
 vim.opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.scrolloff = 10
 vim.opt.termguicolors = true
