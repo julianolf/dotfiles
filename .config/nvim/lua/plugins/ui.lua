a448b27a4159417d7f27f2865aa9b885ae2f262d
Juliano Fernandes <julianofernandes@gmail.com>
2024-03-06 23:49:10 -0300

    Add plugins to better visualize indentation and scope according to cursor position in nvim

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,48 @@
+return {
+   {
+      "lukas-reineke/indent-blankline.nvim",
+      event = { "BufReadPost", "BufNewFile" },
+      opts = {
+         indent = {
+            char = "│",
+            tab_char = "│",
+         },
+         scope = { enabled = false },
+         exclude = {
+            filetypes = {
+               "help",
+               "alpha",
+               "dashboard",
+               "lazy",
+               "mason",
+               "notify",
+            },
+         },
+      },
+      main = "ibl",
+   },
+   {
+      "echasnovski/mini.indentscope",
+      version = false,
+      event = { "BufReadPost", "BufNewFile" },
+      opts = {
+         symbol = "│",
+         options = { try_as_border = true },
+      },
+      init = function()
+         vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+               "help",
+               "alpha",
+               "dashboard",
+               "lazy",
+               "mason",
+               "notify",
+            },
+            callback = function()
+               vim.b.miniindentscope_disable = true
+            end,
+         })
+      end,
+   },
+}
