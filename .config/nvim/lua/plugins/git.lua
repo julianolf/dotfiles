c929aaf5907e7aeb27e1d976b29f44ee30fe5065
Juliano Fernandes <julianofernandes@gmail.com>
2024-03-07 21:50:25 -0300

    Add plugin vim-fugitive for better integration with Git

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
    {
       "lewis6991/gitsigns.nvim",
+      dependencies = "tpope/vim-fugitive",
       opts = {
          on_attach = function(bufnr)
             local gs = package.loaded.gitsigns
